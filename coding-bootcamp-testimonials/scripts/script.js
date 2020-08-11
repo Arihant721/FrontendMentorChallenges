aabcc070727e2882273f8693070db248e2524e24
Arihant Krishnakumar <f20190716@hyderabad.bits-pilani.ac.in>
2020-08-11 23:38:00 +0530

    Update 1

--- coding-bootcamp-testimonials/scripts/script.js
+++ coding-bootcamp-testimonials/scripts/script.js
@@ -1,13 +1,13 @@
 $("#testimonial2").fadeOut(0);
-$(".prev").click(function(event){  
-  $("#testimonial1").fadeToggle(500);
-  $("#testimonial2").fadeToggle(500);
-  $(".prev").css("background-color","black");
-  $(".next").css("background-color","white");
-});
-$(".next").click(function(event){
-  $("#testimonial1").fadeToggle(500);
-  $("#testimonial2").fadeToggle(500);
-  $(".next").css("background-color","black");
+$(".next").click(function(event){  
+  $("#testimonial1").css("display","none");
+  $("#testimonial2").fadeIn(500);
+  $(".next").css("background-color","#eee");
   $(".prev").css("background-color","white");
+});
+$(".prev").click(function(event){
+  $("#testimonial2").css("display","none");
+  $("#testimonial1").fadeIn(500);
+  $(".prev").css("background-color","#eee");
+  $(".next").css("background-color","white");
 });
